18a75c6dda0c6b898d77fd6cbe17ff3fabf50a38
Pranav Vatsa <142565246+pranavv05@users.noreply.github.com>
2025-09-13 23:32:03 +0530

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,5 @@
 import "./global.css";
-import "./dashboard.css"7
+import "./dashboard.css"
 import { Toaster } from "@/components/ui/toaster";
 import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
